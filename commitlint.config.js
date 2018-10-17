227f547109117f2bcb86ff3a79a88232706cec2a
sosout <pss77585211>
2018-10-17 21:04:51 +0800

    feat(v0.0.2): v0.0.2
    v0.0.2

--- commitlint.config.js
+++ commitlint.config.js
@@ -9,10 +9,10 @@ module.exports = {
       'never',
       ['sentence-case', 'start-case', 'pascal-case', 'upper-case']
     ],
-    'subject-empty': [2, 'never'],
+    // 'subject-empty': [2, 'never'],
     'subject-full-stop': [2, 'never', '.'],
     'type-case': [2, 'always', 'lower-case'],
-    'type-empty': [2, 'never'],
+    // 'type-empty': [2, 'never'],
     'type-enum': [
       2,
       'always',
--- packages/index.js
+++ packages/index.js
@@ -12,7 +12,7 @@ import Loading from './loading';
 import Locale from './locale';
 import NavBar from './nav-bar';
 
-const version = '0.0.1';
+const version = '0.0.2';
 const components = [
   Actionsheet,
   Button,
